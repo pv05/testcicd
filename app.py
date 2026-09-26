c54506f0d168ff26ad9b8954ba073862cce7b2e4
pv05 <pankajverma231997@gmail.com>
2026-09-26 12:57:59 +0530

    app.py modified

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify
 
 load_dotenv()
 
-APP_NAME = os.getenv("APP_NAME", "default-app9999")
+APP_NAME = os.getenv("APP_NAME", "default-app787878")
 GREETING = os.getenv("GREETING", "Hello")
 
 app = Flask(__name__)
